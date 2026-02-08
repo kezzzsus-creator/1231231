a3d2a8e2b1f186e5ba4163520f432a98d3cd8c5d
kezzzsus-creator <kezzzsus@gmail.com>
2026-02-08 20:56:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ intents.members = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 TARGET_USER_ID = 385459721139650561
-FORCED_NICK = "Seks-bestia z lipowej 6c 💩🚽"
+FORCED_NICK = "Seks-bestia z lipowej 10c 💩🚽"
 
 @bot.event
 async def on_ready():
