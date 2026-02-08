5e1c01d49d47f5a3b78c168c7fdd07aa2ede05f7
kezzzsus-creator <kezzzsus@gmail.com>
2026-02-08 20:59:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ intents.members = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 TARGET_USER_ID = 385459721139650561
-FORCED_NICK = "Seks-bestia z lipowej 10c 💩🚽"
+FORCED_NICK = "Seks analnej rozkoszi z lipowej 10c 🍒🍑"
 
 @bot.event
 async def on_ready():
